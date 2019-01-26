da80680ff1a3fdeab1c1f0f40ae558aa8ab392e2
fbjoker <39256912@qq.com>
2019-01-26 08:23:15 +0800

    126

--- src/main/scala/com/alex/demo1/DataStreamDemo.scala
+++ src/main/scala/com/alex/demo1/DataStreamDemo.scala
@@ -1,7 +1,8 @@
 package com.alex.demo1
 
-import org.apache.flink.streaming.api.datastream.DataStreamSource
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.streaming.api.scala._
+
 
 object DataStreamDemo {
   def main(args: Array[String]): Unit = {
@@ -16,14 +17,35 @@ object DataStreamDemo {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
     //source 读取文本
-    val stream: DataStreamSource[String] = env.readTextFile("test00.txt")
+    val stream= env.readTextFile("test00.txt")
+    val stream2= env.readTextFile("test01.txt")
+//
+//    //读取socket
+//    val stream2 = env.socketTextStream("hadoop102",4444)
 
-    //读取socket
-    val stream2: DataStreamSource[String] = env.socketTextStream("hadoop102",4444)
+//    val list = List(1,2,3,4)
+
+//   val stream: DataStream[Int] = env.fromCollection(list)
+//    val stream2: DataStream[List[Int]] = env.fromElements(list)
+//    val stream3: DataStream[Long] = env.generateSequence(1,100000000)
 
 
-    stream.print()
-    stream2.print()
+//    val splitWords: DataStream[String] = stream.flatMap(line=> line.split("\\s+"))
+//    val wordsLength: DataStream[String] = splitWords.filter(words=> words.length>6)
+
+
+//    val con: ConnectedStreams[String, String] = stream.connect(stream2)
+//    con.map(x=>println(x),y=>println(y))
+    val words: DataStream[String] = stream.flatMap(line=> line.split("\\s+"))
+    val wordsforcount: DataStream[(String, Int)] = words.map(words=>(words,1))
+
+    val wordsBykey: KeyedStream[(String, Int), Tuple] = wordsforcount.keyBy(0)
+
+    val count: DataStream[(String, Int)] = wordsBykey.reduce( (a1,a2)=> (a1._1,a1._2+a2._2))
+    count.print()
+
+
+
 
     //触发程序执行
     env.execute("demo1")
